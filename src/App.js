9ef10e3a44fef9351e1b4c9e33ca5ab831887573
Jean Carlos Reyes <tibboxcentral@gmail.com>
2022-02-20 17:33:32 -0400

    Add button function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { isEmpty } from 'lodash'
+import shortid from 'shortid';
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (isEmpty(task)) {
+      console.log("Task Empty");
+      return;
+    }
+
+    const newTask={
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([...tasks,newTask]);
+    setTask("");
+  }
   return (
     <div className="container mt-5">
       <h1>Homework</h1>
@@ -10,20 +30,26 @@ function App() {
         <div className="col-8">
         <h4 className="text-center">Homework's List</h4>
         <ul className="list-group">
-        <li className="list-group-item">
-        <span className="lead">Nombre de la tarea</span>
-        <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
-        <button className="btn btn-warning btn-sm float-right">Edit</button>
-        </li>
+        {
+          tasks.map((task) => [
+            <li className="list-group-item" key={task.id}>
+            <span className="lead">{task.name}</span>
+            <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
+            <button className="btn btn-warning btn-sm float-right">Edit</button>
+            </li>
+          ])
+        }
         </ul>
         </div>
       <div className="col-4">
       <h4 className="text-center">Form</h4>
-      <form>
+      <form onSubmit={addTask}>
         <input
         type="text"
         className="form-control mb-2"
         placeholder="Insert the homework..."
+        onChange={(text) => setTask(text.target.value)}
+        value = {task}
         />
         <button
         className="btn btn-dark btn-block"
