d71ba724ce5ff152f495fa7dcd71a6dab953a071
Jean Carlos Reyes <tibboxcentral@gmail.com>
2022-02-20 00:22:55 -0400

    Design the list and the form of homework

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ function App() {
     <div className="container mt-5">
       <h1>Homework</h1>
       <hr/>
-    </div>
+
+      <div className="row">
+        <div className="col-8">
+        <h4 className="text-center">Homework's List</h4>
+        <ul className="list-group">
+        <li className="list-group-item">
+        <span className="lead">Nombre de la tarea</span>
+        <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
+        <button className="btn btn-warning btn-sm float-right">Edit</button>
+        </li>
+        </ul>
+        </div>
+      <div className="col-4">
+      <h4 className="text-center">Form</h4>
+      <form>
+        <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Insert the homework..."
+        />
+        <button
+        className="btn btn-dark btn-block"
+        type="submit"
+        >
+        Agregar
+        </button>
+      </form>
+      </div>
+      </div>
+      </div>
   );
 }
 
